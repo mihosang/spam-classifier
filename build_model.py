366736fca3d43933e4dba5e80f89de82d28f9cf3
Michael Hosang Lee <michael.hosang.lee@sk.com>
2018-10-22 18:35:25 +0900

    modify some logics

--- build_model.py
+++ build_model.py
@@ -1,48 +1,22 @@
 #!/usr/bin/env python
 import json
 from pprint import pprint
+
 import pandas as pd
 from konlpy.tag import Okt
-
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.externals import joblib
-
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+from classifier import Classifier
 
 okt = Okt()
-
-class Classifier():
-
-    def __init__(self, model):
-        self.model = model
-
-    def train(self, x_train, y_train):
-        self.model.fit(x_train, y_train)
-
-    def predict(self, x_test, y_test):
-        self.y_test = y_test
-        self.prediction = self.model.predict(x_test)
-        self.score = accuracy_score(y_test, self.prediction)
-
-    def get_score(self):
-        return self.score
-
-    def print_report(self):
-        print(" 1. Classification Report")
-        print(classification_report(self.y_test, self.prediction, target_names=["Ham", "Spam"]))
-        print(" 2. Confusion Report")
-        conf_mat = confusion_matrix(self.y_test, self.prediction)
-        print(conf_mat)
-
-    def save_model(self, model_file):
-        joblib.dump(self.model, model_file)
 
 def import_spam_data(path):
     data = pd.read_csv(path, encoding='latin-1')
@@ -112,11 +86,12 @@ def get_document_term_matrix_korean(X_train, X_test):
     # vect = CountVectorizer(stop_words=stopwords_korean, ngram_range=(1, 1))
     vect = CountVectorizer()
     vect.fit(X_train)
+    joblib.dump(vect, 'vectorizer.joblib')
 
     X_train_df = vect.transform(X_train)
     X_test_df = vect.transform(X_test)
 
-    return X_train_df, X_test_df
+    return vect, X_train_df, X_test_df
 
 def get_document_term_matrix(X_train, X_test):
     # vect = CountVectorizer(stop_words=stopwords.words('english'), ngram_range=(1,1))
@@ -126,7 +101,7 @@ def get_document_term_matrix(X_train, X_test):
     X_train_df = vect.transform(X_train)
     X_test_df = vect.transform(X_test)
 
-    return X_train_df, X_test_df
+    return vect, X_train_df, X_test_df
 
 if __name__ == "__main__":
 
@@ -142,8 +117,8 @@ if __name__ == "__main__":
     # STEP 2. Split into Train & Test sets
     X_train, X_test, y_train, y_test = train_test_split(data["text"], data["label"], test_size=0.2, random_state=10)
     # STEP 3. Text Transformation
-    # X_train_df, X_test_df = get_document_term_matrix(X_train, X_test)
-    X_train_df, X_test_df = get_document_term_matrix_korean(X_train, X_test)
+    # vect, X_train_df, X_test_df = get_document_term_matrix(X_train, X_test)
+    vect, X_train_df, X_test_df = get_document_term_matrix_korean(X_train, X_test)
     # STEP 4. Classifiers
 
     all_models = [
@@ -160,7 +135,7 @@ if __name__ == "__main__":
         classifier = Classifier(model[1])
         classifier.train(X_train_df, y_train)
         classifier.predict(X_test_df, y_test)
-        classifier.save_model("./data/%s-model.pkl" % model[0])
+        classifier.save_model(vect, "./data/%s-model.pkl" % model[0])
         scores.append({"model":model[0], "score": float(classifier.get_score())})
 
     sorted_scores = sorted(scores, key=lambda k: k['score'], reverse=True)

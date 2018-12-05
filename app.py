34ff6d39e722a7c09760215f30deba2f3d9d8151
Michael Hosang Lee <michael.hosang.lee@sk.com>
2018-12-05 13:38:38 +0900

    fix performance issues

--- app.py
+++ app.py
@@ -13,39 +13,50 @@ max_len = 150
 app = Flask(__name__)
 api = Api(app)
 
+lstm_model = None
+lstm_tok = None
+svc_model = None
+svc_vect = None
+
+def load_model():
+    global lstm_model, lstm_tok, svc_model, svc_vect
+
+    with open('data/model/lstm_model.json', 'r') as handle:
+        json_file = handle.read()
+
+    lstm_model = model_from_json(json_file)
+    lstm_model.load_weights('data/model/lstm_model.h5')
+
+    svc_model = joblib.load('data/model/C-SVC-model.pkl')
+    svc_vect = joblib.load('data/model/vectorizer.joblib')
+    with open('data/model/tokenizer.pickle', 'rb') as handle:
+        lstm_tok = pickle.load(handle)
+    print("load model...")
+
+def lstm_predict(text):
+
+    sequence_matrix = sequence.pad_sequences(lstm_tok.texts_to_sequences(text), maxlen=max_len)
+    prediction = lstm_model.predict(sequence_matrix)
+    l_predict_proba = []
+    for i in range(0, len(text)):
+        l_predict_proba.append(prediction[i][0])
+    predict = ["phishing" if x > 0.5 else "normal" for x in l_predict_proba]
+    l_predict_proba = [str(x) for x in l_predict_proba]
+
+    return predict, l_predict_proba
+
+def svc_predict(text):
+    x_test = svc_vect.transform(text)
+    predict = svc_model.predict(x_test)
+    predict_proba = svc_model.predict_proba(x_test)
+    l_predict_proba = []
+    for i in range(0, len(text)):
+        l_predict_proba.append(predict_proba[i][1])
+    predict = ["phishing" if x == 1 else "normal" for x in predict]
+    l_predict_proba = [str(x) for x in l_predict_proba]
+    return predict, l_predict_proba
+
 class PredictionResource(Resource):
-    def __init__(self):
-        with open('data/model/lstm_model.json', 'r') as handle:
-            self.json_file = handle.read()
-        self.model = model_from_json(self.json_file)
-        self.model.load_weights('data/model/lstm_model.h5')
-        with open('data/model/tokenizer.pickle', 'rb') as handle:
-            self.tok = pickle.load(handle)
-        self.svc_model = joblib.load('data/model/C-SVC-model.pkl')
-        self.vect = joblib.load('data/model/vectorizer.joblib')
-
-    def svc_predict(self, text):
-        x_test = self.vect.transform(text)
-        predict = self.svc_model.predict(x_test)
-        predict_proba = self.svc_model.predict_proba(x_test)
-        l_predict_proba = []
-        for i in range(0, len(text)):
-            l_predict_proba.append(predict_proba[i][1])
-        predict = ["phishing" if x == 1 else "normal" for x in predict]
-        l_predict_proba = [str(x) for x in l_predict_proba]
-        return predict, l_predict_proba
-
-    def lstm_predict(self, text):
-
-        sequence_matrix = sequence.pad_sequences(self.tok.texts_to_sequences(text), maxlen=max_len)
-        prediction = self.model.predict(sequence_matrix)
-        l_predict_proba = []
-        for i in range(0, len(text)):
-            l_predict_proba.append(prediction[i][0])
-        predict = ["phishing" if x > 0.5 else "normal" for x in l_predict_proba]
-        l_predict_proba = [str(x) for x in l_predict_proba]
-
-        return predict, l_predict_proba
 
     def post(self):
         try:
@@ -64,25 +75,21 @@ class PredictionResource(Resource):
                 return jsonify({"error": "text field is required as string or list of string"})
 
             if model == 'svc':
-                svc_predict, svc_predict_proba = self.svc_predict(l_text)
-                result = jsonify({"result": svc_predict, "probability": svc_predict_proba})
+                s_predict, s_predict_proba = svc_predict(l_text)
+                result = jsonify({"result": s_predict, "probability": s_predict_proba})
             elif model == 'lstm':
-                lstm_predict, lstm_predict_proba = self.lstm_predict(l_text)
-                result = jsonify({"result": lstm_predict, "probability": lstm_predict_proba})
+                l_predict, l_predict_proba = lstm_predict(l_text)
+                result = jsonify({"result": l_predict, "probability": l_predict_proba})
             else:
-                svc_predict, svc_predict_proba = self.svc_predict(l_text)
-                lstm_predict, lstm_predict_proba = self.lstm_predict(l_text)
+                s_predict, s_predict_proba = svc_predict(l_text)
+                l_predict, l_predict_proba = lstm_predict(l_text)
 
                 result = jsonify(
                     {
-                        "svc": {"result": svc_predict, "probability": svc_predict_proba},
-                        "lstm": {"result": lstm_predict, "probability": lstm_predict_proba}
+                        "svc": {"result": s_predict, "probability": s_predict_proba},
+                        "lstm": {"result": l_predict, "probability": l_predict_proba}
                      }
                 )
-
-            from keras import backend as K
-            K.clear_session()
-
             return result
 
         except Exception as err:
@@ -108,4 +115,5 @@ api.add_resource(PredictionResource, '/predict')
 api.add_resource(TrainingResource, '/train')
 
 if __name__ == '__main__':
+    load_model()
     app.run(host='0.0.0.0', port=8000)

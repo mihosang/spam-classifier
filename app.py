37d09de6f1da9046e153a938afe4cb6050696483
Michael Hosang Lee <michael.hosang.lee@sk.com>
2018-12-05 14:02:09 +0900

    fix bugs

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pickle
 
-from flask import Flask, jsonify
-from flask_restful import Resource, Api, request, reqparse
+from flask import Flask, jsonify, request
 from keras.engine.saving import model_from_json
 from sklearn.externals import joblib
 from build_model import tokenizer
@@ -11,16 +10,18 @@ max_words = 1000
 max_len = 150
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 
 lstm_model = None
 lstm_tok = None
 svc_model = None
 svc_vect = None
+graph = None
 
 def load_model():
-    global lstm_model, lstm_tok, svc_model, svc_vect
-
+    global lstm_model, lstm_tok, svc_model, svc_vect, graph
+    import tensorflow as tf
+    graph = tf.get_default_graph()
     with open('data/model/lstm_model.json', 'r') as handle:
         json_file = handle.read()
 
@@ -36,7 +37,10 @@ def load_model():
 def lstm_predict(text):
 
     sequence_matrix = sequence.pad_sequences(lstm_tok.texts_to_sequences(text), maxlen=max_len)
-    prediction = lstm_model.predict(sequence_matrix)
+
+    with graph.as_default():
+        prediction = lstm_model.predict(sequence_matrix)
+
     l_predict_proba = []
     for i in range(0, len(text)):
         l_predict_proba.append(prediction[i][0])
@@ -56,63 +60,43 @@ def svc_predict(text):
     l_predict_proba = [str(x) for x in l_predict_proba]
     return predict, l_predict_proba
 
-class PredictionResource(Resource):
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        body = request.get_json()
+        text = body['text']
+        if 'model' in body:
+            model = body['model']
+        else:
+            model = 'all'
 
-    def post(self):
-        try:
-            body = request.get_json()
-            text = body['text']
-            if 'model' in body:
-                model = body['model']
-            else:
-                model = 'all'
+        if type(text) is str:
+            l_text = [text]
+        elif type(text) is list:
+            l_text = text
+        else:
+            return jsonify({"error": "text field is required as string or list of string"})
 
-            if type(text) is str:
-                l_text = [text]
-            elif type(text) is list:
-                l_text = text
-            else:
-                return jsonify({"error": "text field is required as string or list of string"})
+        if model == 'svc':
+            s_predict, s_predict_proba = svc_predict(l_text)
+            return jsonify({"result": s_predict, "probability": s_predict_proba})
+        elif model == 'lstm':
+            l_predict, l_predict_proba = lstm_predict(l_text)
+            return jsonify({"result": l_predict, "probability": l_predict_proba})
+        else:
+            s_predict, s_predict_proba = svc_predict(l_text)
+            l_predict, l_predict_proba = lstm_predict(l_text)
 
-            if model == 'svc':
-                s_predict, s_predict_proba = svc_predict(l_text)
-                result = jsonify({"result": s_predict, "probability": s_predict_proba})
-            elif model == 'lstm':
-                l_predict, l_predict_proba = lstm_predict(l_text)
-                result = jsonify({"result": l_predict, "probability": l_predict_proba})
-            else:
-                s_predict, s_predict_proba = svc_predict(l_text)
-                l_predict, l_predict_proba = lstm_predict(l_text)
+            return jsonify(
+                {
+                    "svc": {"result": s_predict, "probability": s_predict_proba},
+                    "lstm": {"result": l_predict, "probability": l_predict_proba}
+                 }
+            )
 
-                result = jsonify(
-                    {
-                        "svc": {"result": s_predict, "probability": s_predict_proba},
-                        "lstm": {"result": l_predict, "probability": l_predict_proba}
-                     }
-                )
-            return result
-
-        except Exception as err:
-            print(err)
-            return jsonify({"error": str(err)})
-
-class TrainingResource(Resource):
-    def post(self):
-        try:
-            parser = reqparse.RequestParser()
-            parser.add_argument('text')
-            parser.add_argument('label')
-            args = parser.parse_args()
-
-            text, label = args['text'], args['label']
-            return jsonify({"result":"ok"})
-        except Exception as err:
-            print(err)
-            return jsonify({"error": "unknown error"})
-        pass
-
-api.add_resource(PredictionResource, '/predict')
-api.add_resource(TrainingResource, '/train')
+    except Exception as err:
+        print(err)
+        return jsonify({"error": str(err)})
 
 if __name__ == '__main__':
     load_model()
